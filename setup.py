452e26e26120b779fc5c97065bbfaecb08fa7cd9
Tilman <tilman_work@tilmans-imac.home>
2021-02-03 11:31:19 +0100

    added setup requirements

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from setuptools import setup, find_packages
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
+
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
