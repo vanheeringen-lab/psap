425ae14708b53fb5ec2ac342c86d8e29ed3b7236
Tilman <tilman_work@tilmans-imac.home>
2021-02-22 09:57:00 +0100

    Added input validation

--- psap/cli.py
+++ psap/cli.py
@@ -9,23 +9,36 @@ import psap
 def main():
     """Console script for psap."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("-v", "--version", action="version", version=psap.__version__)
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=psap.__version__)
     parser.add_argument(
         "-dbf",
         "--db_fasta",
+        default=None,
+        required=True,
+        help='Path to proteome fasta file'
     )
     parser.add_argument(
         "-o",
         "--out",
+        default="~",
+        required=False,
+        help='Output directory for serialized training-set'
     )
     parser.add_argument(
-        "-id",
-        "--id_name",
+        "-cc",
+        "--class_column",
+        default=None,
+        required=True,
+        help="class column name for training-set"
     )
     args = parser.parse_args()
     basename = Path(args.db_fasta).stem
     # Pickle training-set
-    export_matrix(basename, args.db_fasta, args.out, args.id_name)
+    export_matrix(basename, args.db_fasta, args.out, args.class_cloumn)
 
 
 if __name__ == "__main__":
