1f164bc95830e850f8a10a8a9af643b330b8b281
Tilman <tilman_work@tilmans-imac.home>
2021-03-31 14:54:07 +0200

    updated reference

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
-    description="CLI interface for the PSAP classifier, Mierlo, G. van. Predicting protein condensate formation using machine learning (Manuscript in Preparation).",
+    description="CLI interface for the PSAP classifier (Mierlo, G. van van Mierlo, G., Jansen, J. R. G., Wang, J., Poser, I., van Heeringen, S. J., & Vermeulen, M. (2021). Predicting protein condensate formation using machine learning. Cell Reports, 34(5), 108705. https://doi.org/10.1016/j.celrep.2021.108705)",
     entry_points={
         "console_scripts": [
             "psap=psap.cli:main",
