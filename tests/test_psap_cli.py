9fa821e54f6e6431b4b33623ae5061e448d6c76b
Tilman Schäfers <t.schaefers@science.ru.nl>
2021-04-16 13:55:52 +0200

    refactor

--- tests/test_psap_cli.py
+++ tests/test_psap_cli.py
@@ -6,7 +6,7 @@
 import unittest
 import sklearn_json as skljson
 from pathlib import Path
-
+from psap.matrix import MakeMatrix
 class TestPsap(unittest.TestCase):
     """Tests for `psap` package."""
 
@@ -20,11 +20,24 @@ class TestPsap(unittest.TestCase):
         wl_internal =  Path(__file__).parent.parent / "psap/data/assets/uniprot_ids.txt"
         self.assertTrue (Path(wl_internal).is_file())
     
-    def test_model_features(self):
-        """After importing the annotated data frame, the number of features should equal 87"""
+    def test_fasta_exists(self):
+        fasta =  Path(__file__).parent.parent / "psap/data/testing/testset.fasta"
+        self.assertTrue (Path(fasta).is_file())
+    
+    def test_make_matrix(self):
+        fasta =  Path(__file__).parent.parent / "psap/data/testing/testset.fasta"
+        matrix = MakeMatrix(fasta)
+        self.assertEqual (len(matrix.df.columns),91)
+        
+    def test_feature_equal(self):
+        fasta =  Path(__file__).parent.parent / "psap/data/testing/testset.fasta"
         model_internal = Path(__file__).parent.parent / "psap/data/model/UP000005640_9606_llps.json"
+        matrix = MakeMatrix(fasta)
         clf = skljson.from_json(model_internal)
-        self.assertEqual (clf.n_features_,87)
+        self.assertEqual (clf.n_features_,len(matrix.df.columns)-4, "different feature dimension between training/testset")   
+       
+    
+
     
 
             
