b6ec287f56e163fedb122a2319f1cd68ddf5525d
Tilman Schäfers <t.schaefers@science.ru.nl>
2021-04-16 12:51:33 +0200

    added tests

--- tests/test_psap_cli.py
+++ tests/test_psap_cli.py
@@ -4,16 +4,32 @@
 
 
 import unittest
-
+import sklearn_json as skljson
+from pathlib import Path
 
 class TestPsap(unittest.TestCase):
     """Tests for `psap` package."""
 
-    def setUp(self):
-        """Set up test fixtures, if any."""
+    def test_model_exists(self):
+        """Verify that the default model is part of the installation."""
+        model_internal = Path(__file__).parent.parent / "psap/data/model/UP000005640_9606_llps.json"
+        self.assertTrue (Path(model_internal).is_file())
+    
+    def test_wl_exists(self):
+        """Verify that the default whitelist is part of the installation."""
+        wl_internal =  Path(__file__).parent.parent / "psap/data/assets/uniprot_ids.txt"
+        self.assertTrue (Path(wl_internal).is_file())
+    
+    def test_model_features(self):
+        """After importing the annotated data frame, the number of features should equal 87"""
+        model_internal = Path(__file__).parent.parent / "psap/data/model/UP000005640_9606_llps.json"
+        clf = skljson.from_json(model_internal)
+        self.assertEqual (clf.n_features_,87)
+    
 
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
-
-    def test_000_something(self):
-        """Test something."""
+            
+        
+        
+        
+        
+        
